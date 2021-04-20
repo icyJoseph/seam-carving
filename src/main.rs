ac90bc8cb790b4cd4f9a119a8a9044c3c9d24ff1
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-04-20 14:50:43 +0200

    refactor: Use less type casting where possible - decouple from `image`

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,15 @@ fn in_bounds(x: usize, y: usize, grid: &Vec<Vec<BarePixel>>) -> bool {
     }
 }
 
-fn calc_pixel_energy(x: u32, y: u32, img: &Vec<Vec<BarePixel>>) -> u64 {
-    let y_i = y as usize;
-    let x_i = x as usize;
-
-    let middle = img[y_i][x_i];
+fn calc_pixel_energy(x: usize, y: usize, img: &Vec<Vec<BarePixel>>) -> u64 {
+    let middle = img[y][x];
 
     let r_m = middle.r as i64;
     let g_m = middle.g as i64;
     let b_m = middle.b as i64;
 
-    let left = if x_i > 0 && in_bounds(x_i - 1, y_i, &img) {
-        let rgba_l = img[y_i][x_i - 1];
+    let left = if x > 0 && in_bounds(x - 1, y, &img) {
+        let rgba_l = img[y][x - 1];
         let r_l = rgba_l.r as i64;
         let g_l = rgba_l.g as i64;
         let b_l = rgba_l.b as i64;
@@ -33,8 +30,8 @@ fn calc_pixel_energy(x: u32, y: u32, img: &Vec<Vec<BarePixel>>) -> u64 {
         0
     };
 
-    let right = if in_bounds(x_i + 1, y_i, &img) {
-        let rgba_r = img[y_i][x_i + 1];
+    let right = if in_bounds(x + 1, y, &img) {
+        let rgba_r = img[y][x + 1];
         let r_r = rgba_r.r as i64;
         let g_r = rgba_r.g as i64;
         let b_r = rgba_r.b as i64;
@@ -63,7 +60,11 @@ struct SeamEnergy {
     previous: Option<(usize, usize)>,
 }
 
-fn find_low_energy_seam(energies: &Vec<Vec<u64>>, width: u32, height: u32) -> Vec<(u32, u32)> {
+fn find_low_energy_seam(
+    energies: &Vec<Vec<u64>>,
+    width: usize,
+    height: usize,
+) -> Vec<(usize, usize)> {
     let mut seam_energies: Vec<Vec<SeamEnergy>> = vec![];
 
     let mut seed = vec![];
@@ -80,15 +81,16 @@ fn find_low_energy_seam(energies: &Vec<Vec<u64>>, width: u32, height: u32) -> Ve
 
     seam_energies.push(seed);
 
-    for y in 1..(height as usize) {
+    for y in 1..height {
         let mut row = vec![];
-        for x in 0..(width as usize) {
+
+        for x in 0..width {
             let mut min_prev = 625;
             let mut min_prev_x = x;
 
-            if x > 0 && x < (width as usize) - 1 {
+            if x > 0 && x < width - 1 {
                 for i in x - 1..x + 2 {
-                    if i < (width as usize) && seam_energies[y - 1][i].energy < min_prev {
+                    if i < width && seam_energies[y - 1][i].energy < min_prev {
                         min_prev = seam_energies[y - 1][i].energy;
                         min_prev_x = i;
                     }
@@ -109,8 +111,8 @@ fn find_low_energy_seam(energies: &Vec<Vec<u64>>, width: u32, height: u32) -> Ve
     let mut last_min = None;
     let mut min_seam_energy = 625;
 
-    for x in 0..(width as usize) {
-        let y = (height as usize) - 1;
+    for x in 0..width {
+        let y = height - 1;
         if seam_energies[y][x].energy < min_seam_energy {
             min_seam_energy = seam_energies[y][x].energy;
             last_min = Some((x, y));
@@ -125,9 +127,7 @@ fn find_low_energy_seam(energies: &Vec<Vec<u64>>, width: u32, height: u32) -> Ve
             let mut current = seam_energies[y][x];
 
             loop {
-                let c_x = u32::try_from(current.x).unwrap();
-                let c_y = u32::try_from(current.y).unwrap();
-                seam.push((c_x, c_y));
+                seam.push((current.x, current.y));
 
                 match current.previous {
                     None => return seam,
@@ -146,9 +146,12 @@ fn main() {
         Ok(data) => data.into_rgba8(),
     };
 
-    let (orig_width, height) = rgba_img.dimensions();
+    let (raw_width, raw_height) = rgba_img.dimensions();
 
-    let mut energies = vec![vec![625; orig_width as usize]; height as usize];
+    let orig_width = raw_width as usize;
+    let height = raw_height as usize;
+
+    let mut energies = vec![vec![625; orig_width]; height];
 
     let mut raw_output: Vec<Vec<BarePixel>> = vec![];
 
@@ -170,26 +173,34 @@ fn main() {
 
     for y in 0..height {
         for x in 0..orig_width {
-            energies[y as usize][x as usize] = calc_pixel_energy(x, y, &raw_output);
+            energies[y][x] = calc_pixel_energy(x, y, &raw_output);
         }
     }
 
-    let target = 1920 / 2;
+    let target = 2 * 1920 / 3;
 
     let mut width = orig_width;
 
     loop {
-        println!("Looping, width: {}", width);
         if width == target {
             // end
             // save image
 
-            let mut save_buff = image::ImageBuffer::new(width, height);
+            let to_u32 = |n: usize| match u32::try_from(n) {
+                Ok(m) => m,
+                Err(why) => panic!("Bad usize u32: {:?}", why),
+            };
+
+            let mut save_buff = image::ImageBuffer::new(to_u32(width), to_u32(height));
 
             for y in 0..height {
                 for x in 0..width {
-                    let pixel = raw_output[y as usize][x as usize];
-                    save_buff.put_pixel(x, y, image::Rgba([pixel.r, pixel.g, pixel.b, pixel.a]));
+                    let pixel = raw_output[y][x];
+                    save_buff.put_pixel(
+                        to_u32(x),
+                        to_u32(y),
+                        image::Rgba([pixel.r, pixel.g, pixel.b, pixel.a]),
+                    );
                 }
             }
 
@@ -225,10 +236,7 @@ fn main() {
 
         for (y, row) in buffer.iter().enumerate() {
             for x in 0..row.len() {
-                let e_x = u32::try_from(x).unwrap();
-                let e_y = u32::try_from(y).unwrap();
-
-                energies[y][x] = calc_pixel_energy(e_x, e_y, &buffer);
+                energies[y][x] = calc_pixel_energy(x, y, &buffer);
             }
         }
 
